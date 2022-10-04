7b415398480f3f89c9db10e50b2eb213fc5acc14
Heaming <meetoohi@gmail.com>
2022-10-04 13:55:13 +0900

    [2022/10/04 13:54] Single File Component / Vue@3 login Form - axios

---
+++ vue-cli/vue-project/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{ props }}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+  <nav>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  props: ['props'],
+  methods: {
+    sendEvent: function() {
+      this.$emit('renew');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
